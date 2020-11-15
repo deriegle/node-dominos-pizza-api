ea98d6acb3bad2f0e8417d3b0d45d18fc0a6c206
Devin Riegle <riegledevin@gmail.com>
2020-11-15 11:13:09 -0500

    cleanup

--- src/Item.js
+++ src/Item.js
@@ -1,22 +1,25 @@
 class Item {
   constructor(parameters) {
-    if(!parameters){
-        parameters={}
+    if (!parameters) {
+      parameters = {};
     }
 
     this.AutoRemove=false;
 
-    this.Code = parameters.code||null;
-    this.Qty = parameters.quantity||1;
+    this.Code = parameters.code || null;
+    this.Qty = parameters.quantity || 1;
     this.ID = 1;
     
     this.isNew = true;
-    this.Options = { 'C': {'1/1': '1'}, 'X': {'1/1': '1'} };
+    this.Options = {
+      'C': {'1/1': '1'},
+      'X': {'1/1': '1'},
+    };
 
-    if(parameters.options) {
-        for (var i=0; i<parameters.options.length; i++) {
-            this.Options[parameters.options[i]] = { '1/1': '1' };
-        }
+    if (parameters.options) {
+      Object.values(parameters.options).forEach((value) => {
+        this.Options[value] = { '1/1': '1' };
+      });
     }
   }
 }
