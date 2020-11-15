377e26dc1e86a738595770bb5d9737697b3b6250
Devin Riegle <riegledevin@gmail.com>
2020-11-15 11:44:27 -0500

    wrap methods in a class

--- src/Track.js
+++ src/Track.js
@@ -1,88 +1,86 @@
 const urls = require('../urls.json');
 const parser = require('xml2json');
 
-const byUrl = async (url) => {
-  let result;
+class Track {
+  static async byPhone(phone) {
+    if (!phone) {
+      return {
+        success: false,
+        message: 'Phone is required!'
+      };
+    }
 
-  try {
-    result = await fetch(url)
-  } catch (error) {
-    return {
-      success: false,
-      message: error
-    };
+    return Track._byUrl(`${urls.track}Phone=${phone}`);
   }
 
-  if (!result.ok) {
-    return {
-      success: false,
-      message: `HTML Status Code Error ${result.status}`,
-    };
+  static async byId(storeID, orderKey) {
+    if (!storeID || !orderKey) {
+      return {
+        success: false,
+        message: 'StoreID and orderKey are required!'
+      };
+    }
+
+    return Track._byUrl(`${urls.track}StoreID=${storeID}&OrderKey=${orderKey}`);
   }
 
-  const json = parser.toJson(result.body, {
-    coerce: false,
-    sanitize: false,
-    object: true,
-    trim: false
-  });
+  static async _byUrl(url) {
+    let result;
 
-  if (!json['soap:Envelope']) {
+    try {
+      result = await fetch(url)
+    } catch (error) {
+      return {
+        success: false,
+        message: error
+      };
+    }
+
+    if (!result.ok) {
+      return {
+        success: false,
+        message: `HTML Status Code Error ${result.status}`,
+      };
+    }
+
+    const json = parser.toJson(result.body, {
+      coerce: false,
+      sanitize: false,
+      object: true,
+      trim: false
+    });
+
+    if (!json['soap:Envelope']) {
+      return {
+        success: false,
+        message: 'API soap:Envelope not present',
+        data: result
+      };
+    }
+
+    if (!json['soap:Envelope']['soap:Body']) {
+      return {
+        success: false,
+        message: 'API soap:Body not present',
+        data: result
+      };
+    }
+
+    if(!json['soap:Envelope']['soap:Body'].GetTrackerDataResponse){
+      return {
+        success: false,
+        message:'API GetTrackerDataResponse not present',
+        data: result
+      };
+    }
+
     return {
-      success: false,
-      message: 'API soap:Envelope not present',
-      data: result
+      success: true,
+      orders: json['soap:Envelope']['soap:Body'].GetTrackerDataResponse.OrderStatuses,
+      query: json['soap:Envelope']['soap:Body'].GetTrackerDataResponse.Query
     };
   }
-
-  if (!json['soap:Envelope']['soap:Body']) {
-    return {
-      success: false,
-      message: 'API soap:Body not present',
-      data: result
-    };
-  }
-
-  if(!json['soap:Envelope']['soap:Body'].GetTrackerDataResponse){
-    return {
-      success: false,
-      message:'API GetTrackerDataResponse not present',
-      data: result
-    };
-  }
-
-  return {
-    success: true,
-    orders: json['soap:Envelope']['soap:Body'].GetTrackerDataResponse.OrderStatuses,
-    query: json['soap:Envelope']['soap:Body'].GetTrackerDataResponse.Query
-  };
 }
 
-const byPhone = async (phone) => {
-  if (!phone) {
-    return {
-      success: false,
-      message: 'Phone is required!'
-    };
-  }
-
-  return byUrl(`${urls.track}Phone=${phone}`);
-};
-
-const byId = async (storeID, orderKey) => {
-  if (!storeID || !orderKey) {
-    return {
-      success: false,
-      message: 'StoreID and orderKey are required!'
-    };
-  }
-
-  return byUrl(`${urls.track}StoreID=${storeID}&OrderKey=${orderKey}`);
-};
-
-
-module.exports = {
-  byPhone,
-  byId,
-};
+module.exports = Track;
 
--- src/Utilities.js
+++ src/Utilities.js
@@ -2,40 +2,40 @@ const httpJson = require('./http-json.js');
 const urls = require('../urls.json');
 const Address = require('./Address.js');
 
-const findNearbyStores = async (address, pickUpType, callback) => {
-  if(typeof pickUpType === 'function'){
-    callback = pickUpType;
-    pickUpType = 'Delivery';
-  }
-
-  if(!address || !callback) {
-    if(!callback){
-      throw('invalid findNearbyStores request. address and callback are required at a minimum.');
+class Utilities {
+  static async findNearbyStores(address, pickUpType, callback) {
+    if (typeof pickUpType === 'function') {
+      callback = pickUpType;
+      pickUpType = 'Delivery';
     }
 
-    if(callback) {
-      callback(
-        {
-          success: false,
-          message: 'At least a partial address (minimum accepted is zipcode) is required to find stores'
-        }
-      );
+    if (!address || !callback) {
+      if (!callback) {
+        throw('invalid findNearbyStores request. address and callback are required at a minimum.');
+      }
+
+      if (callback) {
+        callback(
+          {
+            success: false,
+            message: 'At least a partial address (minimum accepted is zipcode) is required to find stores'
+          }
+        );
+      }
+
+      return false;
     }
 
-    return false;
+    address = new Address(address)
+    const addressLines = address.getAddressLines();
+
+    const url = urls.store.find
+      .replace('${line1}', encodeURI(addressLines.line1))
+      .replace('${line2}', encodeURI( addressLines.line2))
+      .replace('${type}', pickUpType || 'Delivery');
+
+    callback(await httpJson.get(url));
   }
+}
 
-  address = new Address(address)
-  const addressLines = address.getAddressLines();
-
-  const url = urls.store.find
-    .replace('${line1}', encodeURI(addressLines.line1))
-    .replace('${line2}', encodeURI( addressLines.line2))
-    .replace('${type}', pickUpType || 'Delivery');
-
-  callback(await httpJson.get(url));
-};
-
-module.exports = {
-  findNearbyStores
-};
+module.exports = Utilities;
