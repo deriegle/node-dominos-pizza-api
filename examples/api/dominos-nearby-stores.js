721abd0766efd37127f3ae91e695612c9c72eaa0
Devin Riegle <riegledevin@gmail.com>
2020-11-15 13:08:05 -0500

    update examples

--- examples/api/dominos-nearby-stores.js
+++ examples/api/dominos-nearby-stores.js
@@ -1,10 +1,10 @@
-var pizzapi = require('../../dominos-pizza-api');
+const { Utilities } = require('../../index');
 
 //Get stores by postal code, distance is not as accurate this way
-pizzapi.Util.findNearbyStores(
-    '63102',
-    'Delivery',
-    function(storeData) {
-        console.log('\n\n##################\nNearby Stores\n##################\n\n',storeData.result.Stores);
-    }
-);
+Utilities.findNearbyStores('63102', 'all').then((result) => {
+  console.log('\n\n##################\nNearby Stores\n##################\n\n');
+  console.log(result.result.Stores);
+})
+.catch((err) => {
+  console.log(err);
+});
