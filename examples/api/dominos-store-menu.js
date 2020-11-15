1d48424ae50922a3255d94a59c9887944b383b4d
Devin Riegle <riegledevin@gmail.com>
2020-11-15 13:18:18 -0500

    Fix print store menu

--- examples/api/dominos-store-menu.js
+++ examples/api/dominos-store-menu.js
@@ -1,38 +1,33 @@
-var pizzapi = require('../../dominos-pizza-api');
+const { Store } = require('../../index');
 
-//Get stores by postal code, distance is not as accurate this way
-pizzapi.Util.findNearbyStores(
-    '63102',
-    'Delivery',
-    function(storeData) {
-        var Store = new pizzapi.Store(
-            {
-                ID: storeData.result.Stores[0].StoreID
-            }
-        );
 
-        //Get Menu for first store
-        Store.getMenu(function(menu) {
-            function printCategory(showItems,category,depth) {
-                if (!depth) depth = 0;
-                var indent = Array(depth+1).join("  ");
-                console.log(indent+category.getName());
-                for (var subIndex in category.getSubcategories()) {
-                    printCategory(showItems,category.getSubcategories()[subIndex],depth+1);
-                }
-                if (showItems) {
-                    category.getProducts().forEach(function(product) {
-                        console.log(indent+"  ["+product.getCode()+"] "+product.getName());
-                    });
-                }
-            }
+function printCategory(category,depth) {
+  if (!depth) depth = 0;
 
-            console.log("************ Coupon Menu ************");
-            printCategory(true,menu.getCouponCategory(),1);
-            console.log("\n\n************ Preconfigured Menu ************");
-            printCategory(true,menu.getPreconfiguredCategory(),1);
-            console.log("\n\n************ Regular Menu ************");
-            printCategory(true,menu.getFoodCategory(),1);
-        });
-    }
-);
+  const indent = Array(depth+1).join("  ");
+
+  console.log(indent+category.getName());
+
+  for (var subIndex in category.getSubcategories()) {
+      printCategory(category.getSubcategories()[subIndex],depth+1);
+  }
+
+  category.getProducts().forEach(function(product) {
+      console.log(indent+"  ["+product.getCode()+"] "+product.getName());
+  });
+}
+
+async function main() {
+  const store = new Store({ ID: 9760 });
+
+  const menu = await store.getMenu();
+
+  console.log("************ Coupon Menu ************");
+  printCategory(menu.getCouponCategory(),1);
+  console.log("\n\n************ Preconfigured Menu ************");
+  printCategory(menu.getPreconfiguredCategory(),1);
+  console.log("\n\n************ Regular Menu ************");
+  printCategory(menu.getFoodCategory(),1);
+}
+
+main();
