5cc4b1ccee71a0ead73ad0cd56471191e1eb04c1
Devin Riegle <riegledevin@gmail.com>
2020-11-15 13:14:25 -0500

    Add store getInfo fixture

--- examples/api/dominos-nearby-stores.js
+++ examples/api/dominos-nearby-stores.js
@@ -1,7 +1,7 @@
 const { Utilities } = require('../../index');
 
 //Get stores by postal code, distance is not as accurate this way
-Utilities.findNearbyStores('63102', 'all').then((result) => {
+Utilities.findNearbyStores('43202', 'all').then((result) => {
   console.log('\n\n##################\nNearby Stores\n##################\n\n');
   console.log(result.result.Stores);
 })
--- examples/api/dominos-store-data.js
+++ examples/api/dominos-store-data.js
@@ -1,23 +1,11 @@
-var pizzapi = require('../../dominos-pizza-api');
+const { Store } = require('../../index');
 
-//Get stores by postal code, distance is not as accurate this way
-pizzapi.Util.findNearbyStores(
-    '63102',
-    'Delivery',
-    function(storeData) {
-        var Store = new pizzapi.Store(
-            {
-                ID: storeData.result.Stores[0].StoreID
-            }
-        );
+async function main() {
+  const store = new Store({ ID: '9760' });
 
-        console.log('\n\n##################\nFirst Store Object\n##################\n\n',Store);
+  const storeData = await store.getInfo();
 
-        //Get Info for first store
-        Store.getInfo(
-            function(storeData) {
-                console.log('\n\n##################\nFirst Store Info\n##################\n\n',storeData.result);
-            }
-        );
-    }
-);
+  console.log(storeData);
+}
+
+main();
